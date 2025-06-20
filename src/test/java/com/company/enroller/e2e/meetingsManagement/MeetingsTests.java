5b021996f6dc2016a04390270a9177d76e77b7b1
Lulily13 <bloodyakuma13@yahoo.com>
2025-06-20 22:08:01 +0200

    feat: add e2e testing of adding new meeting

--- src/test/java/com/company/enroller/e2e/meetingsManagement/MeetingsTests.java
+++ src/test/java/com/company/enroller/e2e/meetingsManagement/MeetingsTests.java
@@ -2,33 +2,82 @@ package com.company.enroller.e2e.meetingsManagement;
 
 import com.company.enroller.e2e.BaseTests;
 import com.company.enroller.e2e.Const;
-import com.company.enroller.e2e.authentication.LoginPage;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MeetingsTests extends BaseTests {
 
     WebDriver driver;
-    MeetingsPage page;
-    LoginPage loginPage;
 
     @BeforeEach
     void setup() {
-//        TODO
+        driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
     }
-
 
     @Test
     @DisplayName("[SPOTKANIA.1] The meeting should be added to your meeting list.")
     void addNewMeeting() {
-//        TODO
+        driver.get(Const.HOME_PAGE);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement loginInput = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//label[contains(text(),'Zaloguj')]/following-sibling::input"))
+        );
+        loginInput.sendKeys(Const.USER_I_NAME);
+
+        WebElement loginBtn = driver.findElement(
+                By.xpath("//button[normalize-space()='Wchodzę']")
+        );
+        loginBtn.click();
+
+        WebElement addMeetingBtn = wait.until(
+                ExpectedConditions.elementToBeClickable(
+                        By.xpath("//button[normalize-space()='Dodaj nowe spotkanie']")
+                )
+        );
+        addMeetingBtn.click();
+
+        String title = "Test-" + UUID.randomUUID();
+        String description = "Opis testowy";
+
+        WebElement titleInput = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//form//input[@type='text']")
+                )
+        );
+        titleInput.sendKeys(title);
+
+        WebElement descriptionInput = driver.findElement(
+                By.xpath("//form//textarea")
+        );
+        descriptionInput.sendKeys(description);
+
+        WebElement submitBtn = driver.findElement(
+                By.xpath("//form//button[normalize-space()='Dodaj']")
+        );
+        submitBtn.click();
+
+        boolean found = wait.until(ExpectedConditions.textToBePresentInElementLocated(
+                By.tagName("body"), title
+        ));
+
+        assertThat(found)
+                .as("Nie znaleziono tytułu '%s' w widocznej treści strony", title)
+                .isTrue();
     }
 
-
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
 }
